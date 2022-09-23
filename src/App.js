fa224b781953fa9443d2a8981e3f06b379f15712
ayanbajo <bolatitoayanbajo@gmail.com>
2022-09-22 20:48:00 -0400

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
 
 
-
-function App() {
+function TypesExample() {
   return (
-    <div className="App">
-      <h1>Bootstrap Website</h1>
-     
-    </div>
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
   );
-}
 
-export default App;
+}
+<TypesExample />
+
+export default TypesExample;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Bootstrap Website</h1>
+     
+//     </div>
+//   );
+// }
+
+// export default App;
